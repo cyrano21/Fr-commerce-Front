ec2db8db4f0206f83cac44acf71f4ffde6b84ab9
cyrano21 <louiscyrano@gmail.com>
2024-02-20 20:19:46 +0100

    m

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -4,39 +4,50 @@ import DescriptionBox from '../Components/DescriptionBox.jsx'
 import RelatedProducts from '../Components/RelatedProducts.jsx'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-import { useParams } from 'react-router-dom' // Importez useParams
 
+// export default Product
 const Product = () => {
-  const { productId } = useParams() // Utilisez useParams pour récupérer l'ID du produit de l'URL
-  const [product, setProduct] = useState(null) // État pour stocker les détails du produit
+  const [products, setProducts] = useState([])
+  const [selectedProductId, setSelectedProductId] = useState(null)
 
   useEffect(() => {
-    const fetchProduct = async () => {
+    const fetchProducts = async () => {
       const backendUrl =
         import.meta.env.VITE_REACT_APP_BACKEND_URL || 'http://localhost:5000'
       try {
-        const { data } = await axios.get(`${backendUrl}/products/${productId}`) // Utilisez productId pour faire une requête au backend
-        setProduct(data) // Stockez les détails du produit récupéré dans l'état
+        const { data } = await axios.get(`${backendUrl}/allproducts`)
+        setProducts(data.products)
+
+        console.log('data.Products:', data.products)
+
+        // Exemple : Sélectionnez le premier produit par défaut ou basé sur une certaine logique
+        if (data.products.length > 0) {
+          setSelectedProductId(data.products[0]._id)
+          console.log('Selected productId:', data.products[0]._id)
+        }
       } catch (error) {
-        console.error('Erreur lors de la récupération du produit', error)
+        console.error('Erreur lors de la récupération des produits', error)
       }
     }
 
-    if (productId) {
-      fetchProduct()
-    }
-  }, [productId]) // Ce useEffect dépend de productId
+    fetchProducts()
+  }, [])
+  const selectedProduct = products.find(
+    (product) => product._id === selectedProductId,
+  )
 
-  if (!product) {
+  console.log('selectedProduct:', selectedProduct)
+
+  if (!selectedProduct) {
     return <div>Chargement...</div>
   }
 
   return (
     <div>
-      <Breadcrums product={product} />
-      <ProductDisplay product={product} />
-      <DescriptionBox product={product} />
-      <RelatedProducts productId={productId} />
+      <Breadcrums product={selectedProduct} />
+      <ProductDisplay productId={selectedProductId} />
+      <DescriptionBox />
+      <RelatedProducts productId={selectedProductId} />
     </div>
   )
 }
